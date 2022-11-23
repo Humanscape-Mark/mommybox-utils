d1419cdc57ac83a3a65debd9252005cfd72e0468
Mark <mark@humanscape.io>
2022-11-23 16:54:12 +0900

    refactor: js -> ts 로 변경

--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,9 @@
-import Sequelize from 'sequelize';
+import Sequelize from 'sequelize'
 
 const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
   dialect: 'mysql',
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT || '3306'
-});
+})
 
-export default sequelize;
+export default sequelize
